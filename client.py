020d7bbe5594bd455fe653ae67c1bea66620cd92
daniel <danielsollis2@gmail.com>
2018-03-29 13:05:09 -0700

    fixed connection error bugs

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-from socket import socket, AF_INET, SOCK_STREAM
+from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 from time import sleep
 
 connection_port = 1025
@@ -10,36 +10,40 @@ def main():
     quit_loop = False
     while not quit_loop:
 
-        print "FTP>"
-        ftp_input = raw_input()
-        ftp_input = ftp_input.split()
+        # print "FTP>"
+        # ftp_input = raw_input()
+        # ftp_input = ftp_input.split()
+        #
+        # if len(ftp_input) == 1:
+        #     if ftp_input[0] == "ls":
+        #             ls()
+        #     elif ftp_input[0] == "quit":
+        #             quit_loop = True
+        #
+        # elif len(ftp_input) == 2:
+        #     if ftp_input[0] == "get":
+        #         get(ftp_input[1])
+        #
+        #     elif ftp_input[0] == "put":
+        #         put(ftp_input[1])
+        #     else:
+        #         print ftp_input[0] + "is not a recognized command"
+        #
+        # else:
+        #     print "Unrecognized set of commands"
 
-        if len(ftp_input) == 1:
-            if ftp_input[0] == "ls":
-                    ls()
-            elif ftp_input[0] == "quit":
-                    quit_loop = True
-
-        elif len(ftp_input) == 2:
-            if ftp_input[0] == "get":
-                get(ftp_input[1])
-
-            elif ftp_input[0] == "put":
-                put(ftp_input[1])
-            else:
-                print ftp_input[0] + "is not a recognized command"
-
-        else:
-            print "Unrecognized set of commands"
-
+        sleep(0.005)
         connection_socket = socket(AF_INET, SOCK_STREAM)
+        connection_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         # two parentheses due to connect() only accepting one argument and requiring a tuple
         connection_socket.connect((server_host, connection_port))
 
+        sleep(0.005)
         data = "Hello World!"
         data_length = len(data)
 
         data_socket = socket(AF_INET, SOCK_STREAM)
+        data_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         data_socket.connect((server_host, data_port))
         data_socket.send(str(data_length))
 
@@ -65,6 +69,7 @@ def get(filename):
 
 def put(filename):
     print filename
+
 
 if __name__ == '__main__':
     main()
