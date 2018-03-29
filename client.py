4c1b6c13ea699433b008b403ebd4c86e7a862af4
daniel <danielsollis2@gmail.com>
2018-03-29 13:40:35 -0700

    made helper functions to simplify main

--- client.py
+++ client.py
@@ -7,35 +7,18 @@ server_host = ''
 
 
 def main():
+    connection_socket = create_connection_socket()
     quit_loop = False
     while not quit_loop:
 
-        print "FTP>"
-        ftp_input = raw_input()
-        ftp_input = ftp_input.split()
+        ftp_input = get_input()
 
         if len(ftp_input) == 1:
-            if ftp_input[0] == "ls":
-                    ls()
-            elif ftp_input[0] == "quit":
-                    quit_loop = True
-
+            quit_loop = len_one_input(ftp_input)
         elif len(ftp_input) == 2:
-            if ftp_input[0] == "get":
-                get(ftp_input[1])
-
-            elif ftp_input[0] == "put":
-                put(ftp_input[1])
-            else:
-                print ftp_input[0] + "is not a recognized command"
-
+            len_two_input(ftp_input)
         else:
             print "Unrecognized set of commands"
-
-        connection_socket = socket(AF_INET, SOCK_STREAM)
-        connection_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        # two parentheses due to connect() only accepting one argument and requiring a tuple
-        connection_socket.connect((server_host, connection_port))
 
         #to give connection_socket time to connect
         sleep(0.005)
@@ -43,21 +26,62 @@ def main():
         data = "Hello World!"
         data_length = len(data)
 
-        data_socket = socket(AF_INET, SOCK_STREAM)
-        data_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        data_socket = create_data_socket()
         data_socket.connect((server_host, data_port))
         data_socket.send(str(data_length))
 
         #to keep second data transmission (data) from being part of first (data_length)
         sleep(0.005)
 
-        data_sent = 0
-
-        while data_sent != len(data):
-            data_sent += data_socket.send(data[data_sent])
-
+        send_data(data, data_socket)
         data_socket.close()
-        connection_socket.close()
+    connection_socket.close()
+
+
+def create_connection_socket():
+    connection_socket = socket(AF_INET, SOCK_STREAM)
+    connection_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    # two parentheses due to connect() only accepting one argument and requiring a tuple
+    connection_socket.connect((server_host, connection_port))
+    return connection_socket
+
+
+def get_input():
+    print "FTP>"
+    ftp_input = raw_input()
+    ftp_input = ftp_input.split()
+    return ftp_input
+
+
+def len_one_input(ftp_input):
+    quit_loop = False
+    if ftp_input[0] == "ls":
+        ls()
+    elif ftp_input[0] == "quit":
+        quit_loop = True
+    return quit_loop
+
+
+def len_two_input(ftp_input):
+    if ftp_input[0] == "get":
+        get(ftp_input[1])
+
+    elif ftp_input[0] == "put":
+        put(ftp_input[1])
+    else:
+        print ftp_input[0] + "is not a recognized command"
+
+
+def create_data_socket():
+    data_socket = socket(AF_INET, SOCK_STREAM)
+    data_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    return data_socket
+
+
+def send_data(data, data_socket):
+    data_sent = 0
+    while data_sent != len(data):
+        data_sent += data_socket.send(data[data_sent])
 
 
 def ls():
